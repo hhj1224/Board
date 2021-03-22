cc7e9c47f5c092adee82e019d0ed09e48275c2de
hhj1224 <hhj5961@gmail.com>
2021-03-22 16:50:54 +0900

    0322 compeletgit add .

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,43 @@
 // public/js/script.js
 
 $(function(){
-    function get2digits (num){
-      return ('0' + num).slice(-2);
-    }
-  
-    function getDate(dateObj){
-      if(dateObj instanceof Date)
-        return dateObj.getFullYear() + '-' + get2digits(dateObj.getMonth()+1)+ '-' + get2digits(dateObj.getDate());
-    }
-  
-    function getTime(dateObj){
-      if(dateObj instanceof Date)
-        return get2digits(dateObj.getHours()) + ':' + get2digits(dateObj.getMinutes())+ ':' + get2digits(dateObj.getSeconds());
-    }
-  
-    function convertDate(){
-      $('[data-date]').each(function(index,element){
-        var dateString = $(element).data('date');
-        if(dateString){
-          var date = new Date(dateString);
-          $(element).html(getDate(date));
-          console.log(date);
-        }
-      });
-    }
-  
-    function convertDateTime(){
-      $('[data-date-time]').each(function(index,element){
-        var dateString = $(element).data('date-time');
-        if(dateString){
-          var date = new Date(dateString);
-          $(element).html(getDate(date)+' '+getTime(date));
-        }
-      });
-    }
-  
-    convertDate();
-    convertDateTime();
+  function get2digits (num){
+    return ('0' + num).slice(-2);
+  }
+
+  function getDate(dateObj){
+    if(dateObj instanceof Date)
+      return dateObj.getFullYear() + '-' + get2digits(dateObj.getMonth()+1)+ '-' + get2digits(dateObj.getDate());
+  }
+
+  function getTime(dateObj){
+    if(dateObj instanceof Date)
+      return get2digits(dateObj.getHours()) + ':' + get2digits(dateObj.getMinutes())+ ':' + get2digits(dateObj.getSeconds());
+  }
+
+  function convertDate(){
+    $('[data-date]').each(function(index,element){
+      var dateString = $(element).data('date');
+      if(dateString){
+        var date = new Date(dateString);
+        $(element).html(getDate(date));
+        console.log(date);
+      }
+    });
+  }
+
+  function convertDateTime(){
+    $('[data-date-time]').each(function(index,element){
+      var dateString = $(element).data('date-time');
+      if(dateString){
+        var date = new Date(dateString);
+        $(element).html(getDate(date)+' '+getTime(date));
+      }
+    });
+  }
+
+  convertDate();
+  convertDateTime();
 });
 
 $(function(){
@@ -71,4 +71,31 @@ $(function(){
       }
     });
   }
+});
+
+$(function(){
+  function resetTitleEllipsisWidth(){
+    $('.board-table .title-text').each(function(i,e){
+      var $text = $(e);
+      var $ellipsis = $(e).closest('.title-ellipsis');
+      var $comment = $(e).closest('.title-container').find('.title-comments');
+
+      var textWidth = $text.width();
+      var ellipsisWidth = $ellipsis.outerWidth();
+      var commentWidth = $comment.outerWidth();
+      var padding = 1;
+
+      if(ellipsisWidth <= (textWidth+commentWidth+padding)){
+        $ellipsis.width(ellipsisWidth-(commentWidth+padding));
+      }
+      else {
+        $ellipsis.width(textWidth+padding);
+      }
+    });
+  }
+  $(window).resize(function(){
+    $('.board-table .title-ellipsis').css('width','');
+    resetTitleEllipsisWidth();
+  });
+  resetTitleEllipsisWidth();
 });
--- routes/posts.js
+++ routes/posts.js
@@ -3,9 +3,12 @@
 const { json } = require('body-parser');
 var express  = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({ dest: 'uploadedFiles/' });
 var Post = require('../models/Post');
 var User = require('../models/User');
 var Comment = require('../models/Comment');
+var File = require('../models/File');
 const util = require('../util');
 
 // Index 
@@ -21,14 +24,47 @@ router.get('/', async function(req, res){
   var posts = [];
 
   if(searchQuery){
-  var count = await Post.countDocuments(searchQuery);
-  maxPage = Math.ceil(count/limit);
-  posts = await Post.find(searchQuery)
-    .populate('author')
-    .sort('-createdAt')
-    .skip(skip)
-    .limit(limit)
-    .exec();
+    var count = await Post.countDocuments(searchQuery);
+    maxPage = Math.ceil(count/limit);
+    posts = await Post.aggregate([
+      { $match: searchQuery },
+      { $lookup: {
+          from: 'users',
+          localField: 'author',
+          foreignField: '_id',
+          as: 'author'
+      } },
+      { $lookup: {
+        from: 'files',
+        localField: 'attachment',
+        foreignField: '_id',
+        as: 'attachment'
+      } },
+      { $unwind: {
+        path: '$attachment',
+        preserveNullAndEmptyArrays: true
+      } },
+      { $sort : { createdAt: -1 } },
+      { $skip: skip },
+      { $limit: limit },
+      { $lookup: {
+          from: 'comments',
+          localField: '_id',
+          foreignField: 'post',
+          as: 'comments'
+      } },
+      { $project: {
+          title: 1,
+          author: {
+            username: 1,
+          },
+          views: 1,
+          numId: 1,
+          attachment: { $cond: [{$and: ['$attachment', {$not: '$attachment.isDeleted'}]}, true, false] },
+          createdAt: 1,
+          commentCount: { $size: '$comments'}
+      } },
+    ]).exec();
   }
   res.render('posts/index',{
     posts:posts,
@@ -48,14 +84,26 @@ router.get('/new', util.isLoggedin, function(req, res){
 });
 
 // create
-router.post('/', util.isLoggedin, function(req, res){
+router.post('/', util.isLoggedin, upload.single('attachment'), async function(req, res){ 
+  var attachment;
+  try{
+    attachment = req.file?await File.createNewInstance(req.file, req.user._id):undefined;
+  }
+  catch(err){
+    return res.json(err);
+  }
+  req.body.attachment = attachment;
   req.body.author = req.user._id;
   Post.create(req.body, function(err, post){
     if(err) {
       req.flash('post', req.body);
       req.flash('errors', util.parseError(err));
       return res.redirect('/posts/new'+res.locals.getPostQueryString());
-    };
+    }
+    if(attachment){
+      attachment.postId = post._id;
+      attachment.save();
+    }
     res.redirect('/posts'+res.locals.getPostQueryString(false, {page:1, searchText:''}));
   });
 });
@@ -66,15 +114,16 @@ router.get('/:id', function(req, res){
   var commentError = req.flash('commentError')[0] || {_id:null, parentComment:null, errors:{}};
 
   Promise.all([
-    Post.findOne({_id:req.params.id}).populate({path:'author', select:'username'}),
+    Post.findOne({_id:req.params.id}).populate({path:'author', select:'username'}).populate({path:'attachment',match:{isDeleted:false}}),
     Comment.find({post:req.params.id}).sort('createdAt').populate({path:'author', select:'username'})
   ])
   .then(([post, comments]) => {
+    post.views++;
+    post.save();
     var commentTrees = util.convertToTrees(comments, '_id','parentComment','childComments');
     res.render('posts/show', {post:post, commentTrees:commentTrees, commentForm:commentForm, commentError:commentError});
   })
   .catch((err) => {
-    console.log('err: ', err);
     return res.json(err);
   })
 });
@@ -84,7 +133,9 @@ router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
   var post = req.flash('post')[0];
   var errors = req.flash('errors')[0] || {};
   if(!post){
-    Post.findOne({_id:req.params.id}, function(err, post){
+    Post.findOne({_id:req.params.id})
+    .populate({path:'attachment', match:{isDeleted:false}})
+    .exec(function(err, post){
       if(err) return res.json(err);
       res.render('posts/edit', {post:post, errors:errors});
     });
@@ -96,8 +147,18 @@ router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
 });
 
 // update
-router.put('/:id', util.isLoggedin, checkPermission, function(req, res){
-  req.body.updatedAt = Date.now(); //2
+router.put('/:id', util.isLoggedin, checkPermission, upload.single('newAttachment'), async function(req, res){
+  var post = await Post.findOne({_id:req.params.id}).populate({path:'attachment',match:{isDeleted:false}});
+  if(post.attachment && (req.file || !req.body.attachment)){
+    post.attachment.processDelete();
+  }
+  try{
+    req.body.attachment = req.file?await File.createNewInstance(req.file, req.user._id, req.params.id):post.attachment;
+  }
+  catch(err){
+    return res.json(err);
+  }
+  req.body.updatedAt = Date.now();
   Post.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, post){
     if(err) {
       req.flash('post', req.body);
